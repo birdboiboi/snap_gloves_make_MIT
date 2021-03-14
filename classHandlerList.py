fbc94afae3638cf13bf2d82bfe9a39096be33da5
birdboiboi <jai36@drexel.edu>
2021-03-14 03:41:00 -0400

    added example to classHandlerList on how to connect WIFI

--- classHandlerList.py
+++ classHandlerList.py
@@ -6,6 +6,7 @@ import networkHandler
 def get_list_of_imports():
     #include all scripts like 
     #samplePinRead.SamplePinRead(name = "pinRead1",pin_out = [0,2],pin_in = [5,12,4],pull_down = [5,4]),
-    ls = [networkHandler.NetworkHandler(wifi_name = "SSID",password = "password")
+    ls = [networkHandler.NetworkHandler(wifi_name = "SSID",password = "password"),
+          samplePinRead.SamplePinRead(name = "pinRead1",pin_out = [0,2],pin_in = [5,12,4],pull_down = [5,4]),
         ]
     return ls
